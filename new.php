3723f8dc73558425eef1992027369fbc544c442b
ravi0513 <160619836+ravi0513@users.noreply.github.com>
2024-11-22 23:33:26 +0530

    Update new.php

--- new.php
+++ new.php
@@ -1,12 +1,5 @@
 <?php
-// Create a file named hello_world.php
-$file = 'hello_world.php';
 
-// Content to write into the file
-$content = '<?php echo "Hello, World!"; ?>';
 
-// Write the content to the file
-file_put_contents($file, $content);
-
-echo "File 'hello_world.php' created successfully.";
+echo "test";
 ?>
